d87ea11801dd38831464bcc4a840fa0cce944a66
rezarahimian <mohammadreza.rahimian@polymtl.ca>
2017-04-29 21:55:03 -0400

    29-Apr-2017@9:55PM

--- app/src/main/java/com/inf8405/polymtl/lab3/activities/MainMenuActivity.java
+++ app/src/main/java/com/inf8405/polymtl/lab3/activities/MainMenuActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.inf8405.polymtl.lab3.R;
