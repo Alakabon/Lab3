69517b0d1ecc273d7c44538d931294645b573e90
Alakabon <momoreau552@gmail.com>
2017-04-30 15:02:38 -0400

    no typo in fix this time

--- app/src/main/java/com/inf8405/polymtl/lab3/activities/AddArtworkActivity.java
+++ app/src/main/java/com/inf8405/polymtl/lab3/activities/AddArtworkActivity.java
@@ -98,8 +98,8 @@ public class AddArtworkActivity extends AppCompatActivity {
                 GPSManager.getLatestGPSLocation(getApplicationContext());
                 Location deviceLocation = _gdm.get_deviceLocation();
                 
-                artwork.setGpsX(GPSManager.getDefaultLongitude());
-                artwork.setGpsX(GPSManager.getDefaultLatitude());
+                artwork.setGpsX(deviceLocation.getLongitude());
+                artwork.setGpsY(deviceLocation.getLatitude());
                 
                 artwork.setPhotoURL(encodedPhoto);
                 
